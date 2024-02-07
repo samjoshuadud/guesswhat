749877aa0cb3a89744fb219f4691f4566f65f453
samjoshuadud <joshuaarmojallas123@gmail.com>
2024-02-07 20:02:54 +0800

    first

---
+++ main.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    setTimeout(function() {
+        $('.spinner').fadeOut();
+        $('#preloader').delay(350).fadeOut('slow');
+        $('body').delay(350).css({
+            'overflow': 'visible'
+        });
+    }, 600);
+})
+
+function init() {
+    var xYes = (0.9 * $(window).width() - $('#yes').width() - $('#no').width()) / 2;
+    var xNo = xYes + $('#yes').width() + 0.1 * $(window).width();
+    var y = 0.75 * $(window).height();
+    $('#yes').css("left", xYes);
+    $('#yes').css("top", y);
+
+    $('#no').css("left", xNo);
+    $('#no').css("top", y);
+}
+
+function switchButton() {
+    var leftNo = $('#no').css("left");
+    var topNO = $('#no').css("top");
+    var leftY = $('#yes').css("left");
+    var topY = $('#yes').css("top");
+    $('#no').css("left", leftY);
+    $('#no').css("top", topY);
+    $('#yes').css("left", leftNo);
+    $('#yes').css("top", topNO);
+}
+// move random button position
+function moveButton() {
+    var x = Math.random() * ($(window).width() - $('#no').width()) * 0.9;
+    var y = Math.random() * ($(window).height() - $('#no').height()) * 0.9;
+    var left = x + 'px';
+    var top = y + 'px';
+    $('#no').css("left", left);
+    $('#no').css("top", top);
+}
+
+init()
+
+var n = 0;
+$('#no').mousemove(function() {
+    if (Math.random() < 0.5 || n == 1)
+        switchButton();
+    else
+        moveButton();
+    n++;
+});
+$('#no').click(() => {
+    if (screen.width >= 900)
+        switchButton();
+})
